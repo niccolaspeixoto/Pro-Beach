7eddbd9764dc2237a7477b9429c64b0a3e4f0698
Niccolas Peixoto <niccmello@gmail.com>
2026-02-01 07:50:34 -0300

    Adicionando a troca de imagens no main.

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ const observer = new IntersectionObserver((entries) => {
 elementos.forEach((el) => {
     observer.observe(el);
 });
+
+
+//troca de imagem do main
+
+const imagens = [
+    'https://lh3.googleusercontent.com/p/AF1QipMUDmA1JpYuDmJZg-GjtBwNHhG096sssFQPQ_jM=s680-w680-h510-rw',
+    "./assets/churrasqueira.jpg",
+    "./assets/lanchonete.jpg"
+]
+
+let index = 0;
+const banner = document.querySelector('.background-main');
+
+function trocarImagem() {
+    banner.style.opacity = 0;
+
+    setTimeout(() => {
+    banner.style.backgroundImage = `url(${imagens[index]})`;
+    banner.style.opacity = 0.3;
+  }, 300);
+}
+
+// troca a cada 5 segundos
+setInterval(() => {
+  index++;
+  if (index >= imagens.length) index = 0;
+  trocarImagem();
+}, 5000);
+
+// imagem inicial
+trocarImagem();
