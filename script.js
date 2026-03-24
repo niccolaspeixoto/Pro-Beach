aaebae91b5db0a29d661dd788c6affce2a36610f
Niccolas Peixoto <niccmello@gmail.com>
2026-03-24 20:01:02 -0300

    Alterando fotos do projeto e adicionando um vídeo na section "about-us"

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ elementos.forEach((el) => {
 //troca de imagem do main
 
 const imagens = [
-    'https://lh3.googleusercontent.com/p/AF1QipMUDmA1JpYuDmJZg-GjtBwNHhG096sssFQPQ_jM=s680-w680-h510-rw',
-    "./assets/churrasqueira.jpg",
-    "./assets/lanchonete.jpg"
+    "./assets/lanchonete.jpg",
+    "./assets/quadra-futevolei.jpg",
+    "./assets/churrasqueira-iphone.jpg",
+    "./assets/quadras-futevolei.jpg",
+    "./assets/quadra-society.jpg"
 ]
 
 let index = 0;
@@ -31,7 +33,7 @@ function trocarImagem() {
     setTimeout(() => {
     banner.style.backgroundImage = `url(${imagens[index]})`;
     banner.style.opacity = 0.3;
-  }, 300);
+  }, 500);
 }
 
 // troca a cada 5 segundos
